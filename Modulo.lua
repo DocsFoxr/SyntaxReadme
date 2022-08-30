732c8ed6c4a5379f242ddc50a5d90ded845d2c48
DocsFoxr <68664950+DocsFoxr@users.noreply.github.com>
2022-08-30 18:36:17 -0500

    Create Modulo.lua

---
+++ Modulo.lua
@@ -0,0 +1,2 @@
+local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/DocsFoxr/Droid-Y/main/VersionAlternative.lua', true))();
+local libreria = Framework.ModuleProtect('_K_SecureExploit', <string number> Watch ID) --default 'nil'
